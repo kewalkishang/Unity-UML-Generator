cf42d46ea1cfa89a45e69d974389a23a53ec74e3
Kewal Kishan Gokuldas <kewal@Kewals-MBP.askey.com>
2023-07-02 21:40:29 -0700

    code cleanup

--- Assets/Scripts/ClassDiagram.cs
+++ Assets/Scripts/ClassDiagram.cs
@@ -31,76 +31,11 @@ public class ClassDiagram : MonoBehaviour
 
         Class classInst = new Class("DemoClass", attributes, operations);
 
-        CreateDiagram2(classInst);
+        CreateDiagram(classInst);
 
     }
 
     void CreateDiagram(Class classIns)
-    {
-        //Render Panel
-        GameObject panel = Instantiate(PanelPrefab, Vector3.zero, Quaternion.identity);
-        panel.transform.SetParent(Canvas.transform);
-
-        panel.GetComponent<RectTransform>().anchoredPosition = Vector3.zero;
-
-        Transform content = panel.transform;
-
-
-        //Add Class Name;
-        GameObject nameText = Instantiate(namePrefab, Vector3.zero, Quaternion.identity);
-        nameText.transform.SetParent(content);
-        nameText.GetComponent<TMP_Text>().text = classIns.name;
-        //Position name;
-
-        //panel.GetComponent<RectTransform>().sizeDelta = new Vector2(300, 400);
-
-        GameObject divider = Instantiate(dividerLinePrefab, Vector3.zero, Quaternion.identity);
-        divider.transform.SetParent(content);
-
-        //TextGenerator textGenerator = new TextGenerator();
-
-        Attribute[] attributes = classIns.attributes;
-        for(int i = 0; i < attributes.Length; i++)
-        {
-            GameObject attributeText = Instantiate(AttributePrefab, Vector3.zero, Quaternion.identity);
-            attributeText.transform.SetParent(content);
-            attributeText.GetComponent<TMP_Text>().text = attributes[i].ToUMLStandard();
-           // maxWidth = Mathf.Max(maxWidth, attributeText.GetComponent<TMP_Text>().preferredWidth);
-
-        }
-
-        GameObject dividera = Instantiate(dividerLinePrefab, Vector3.zero, Quaternion.identity);
-        dividera.transform.SetParent(content);
-
-
-        Operation[] operations = classIns.operations;
-        for (int i = 0; i < operations.Length; i++)
-        {
-            GameObject operationText = Instantiate(AttributePrefab, Vector3.zero, Quaternion.identity);
-            operationText.transform.SetParent(content);
-            operationText.GetComponent<TMP_Text>().text = operations[i].ToUMLStandard();
-           // maxWidth = Mathf.Max(maxWidth, operationText.GetComponent<TMP_Text>().preferredWidth);
-
-        }
-        //Debug.Log("Width "+maxWidth);
-        //float wid = panel.GetComponent<RectTransform>().sizeDelta.x;
-        //Debug.Log("Width " + wid);
-        Vector2 newPosition = nameText.GetComponent<RectTransform>().anchoredPosition;
-        //Debug.Log("Pos " + newPosition.x);
-        //Debug.Log("Posadsa " + nameText.GetComponent<RectTransform>().anchoredPosition3D.x);
-        newPosition.x +=  400;
-        nameText.GetComponent<RectTransform>().anchoredPosition = newPosition;
-        //Change width of name , divider
-        // GameObject viewPort = panel.GetComponentInChildren<View>().gameObject
-        // panel.GetComponent<RectTransform>().sizeDelta = new Vector2(maxWidth, 400);
-        //divider.GetComponent<RectTransform>().sizeDelta = new Vector2(maxWidth, 10);
-        //dividera.GetComponent<RectTransform>().sizeDelta = new Vector2(maxWidth, 10);
-
-       
-    }
-
-
-    void CreateDiagram2(Class classIns)
     {
         //Render Panel
         GameObject mainpanel = Instantiate(MainPanelPrefab, Vector3.zero, Quaternion.identity);
@@ -120,12 +55,10 @@ public class ClassDiagram : MonoBehaviour
         GameObject panel = Instantiate(PanelPrefab, Vector3.zero, Quaternion.identity);
         panel.transform.SetParent(content);
         Transform panelcontent = panel.transform;
-        //panel.GetComponent<RectTransform>().sizeDelta = new Vector2(300, 400);
 
         GameObject divider = Instantiate(dividerLinePrefab, Vector3.zero, Quaternion.identity);
         divider.transform.SetParent(panelcontent);
 
-        //TextGenerator textGenerator = new TextGenerator();
 
         Attribute[] attributes = classIns.attributes;
         for (int i = 0; i < attributes.Length; i++)
@@ -133,7 +66,6 @@ public class ClassDiagram : MonoBehaviour
             GameObject attributeText = Instantiate(AttributePrefab, Vector3.zero, Quaternion.identity);
             attributeText.transform.SetParent(panelcontent);
             attributeText.GetComponent<TMP_Text>().text = attributes[i].ToUMLStandard();
-            // maxWidth = Mathf.Max(maxWidth, attributeText.GetComponent<TMP_Text>().preferredWidth);
 
         }
 
@@ -147,23 +79,8 @@ public class ClassDiagram : MonoBehaviour
             GameObject operationText = Instantiate(AttributePrefab, Vector3.zero, Quaternion.identity);
             operationText.transform.SetParent(panelcontent);
             operationText.GetComponent<TMP_Text>().text = operations[i].ToUMLStandard();
-            // maxWidth = Mathf.Max(maxWidth, operationText.GetComponent<TMP_Text>().preferredWidth);
 
         }
-        //Debug.Log("Width "+maxWidth);
-        //float wid = panel.GetComponent<RectTransform>().sizeDelta.x;
-        //Debug.Log("Width " + wid);
-       // Vector2 newPosition = nameText.GetComponent<RectTransform>().anchoredPosition;
-        //Debug.Log("Pos " + newPosition.x);
-        //Debug.Log("Posadsa " + nameText.GetComponent<RectTransform>().anchoredPosition3D.x);
-       // newPosition.x += 400;
-        //nameText.GetComponent<RectTransform>().anchoredPosition = newPosition;
-        //Change width of name , divider
-        // GameObject viewPort = panel.GetComponentInChildren<View>().gameObject
-        // panel.GetComponent<RectTransform>().sizeDelta = new Vector2(maxWidth, 400);
-        //divider.GetComponent<RectTransform>().sizeDelta = new Vector2(maxWidth, 10);
-        //dividera.GetComponent<RectTransform>().sizeDelta = new Vector2(maxWidth, 10);
-
 
     }
   
